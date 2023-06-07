ba9336b9fafc2739965f11a7048e38ea85ebeb94
Nesrine-com <ne.lamouchi@gmail.com>
2023-06-07 20:52:48 +0200

    client

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,10 @@
 const EventController= require('../controllers/EventCotrollers');
 
+
 module.exports=(app)=>{
-    app.get('/api/Events',EventController.findAll)
-    app.post('/api/create',EventController.create)
-    app.get('/api/Events/:id',  EventController.getOne)
+    app.get('/api/Events', EventController.findAll)
+    app.post('/api/create', EventController.create)
+    app.get('/api/Events/:id', EventController.getOne)
     app.delete('/api/delete/:id',  EventController.delete)
     app.put('/api/update/:id',  EventController.update)
 }
